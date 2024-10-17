891ad93825e020cc87d5ceacb9e1e77a26ae6c1d
Rafael Carneiro Pregardier <rafaelpregardier@gmail.com>
2024-10-17 14:13:11 -0300

    a

--- aula6e7e8.hs
+++ aula6e7e8.hs
@@ -1,6 +1,7 @@
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 {-# HLINT ignore "Eta reduce" #-}
 import Data.Char (toUpper)
+import System.Random (randomRIO)
 data Vec3 a = Vec3 a a a
 
 instance Show a => Show (Vec3 a) where
@@ -24,14 +25,35 @@ instance Semigroup Int where
 instance Semigroup a => Semigroup (Meta a) where
     (Meta s1 l1 w1) <> (Meta s2 l2 w2) = Meta (s1 <> s2) (l1 + l2) (w1 + w2)
 
-instance Functor Meta where 
+instance Functor Meta where
     fmap f (Meta c l w ) = Meta (f c) l w
 
 toUpperString :: String -> String
 toUpperString str = fmap toUpper str
+
+somaRandom :: Int -> IO Int
+somaRandom n = do
+    x <- randomRIO (1, 100)
+    return (n + x)
 
 main :: IO ()
 main = do
     str <- readFile "teste.txt"
     let meuMeta = metaFromString str
     writeFile "meta.test" (show $ fmap toUpperString meuMeta)
+
+
+listTest :: [Int]
+listTest = do
+    x <- [1, -1]
+    y <- [1..10]
+    return (x + y)
+
+safeDiv :: Float -> Float -> Maybe Float
+safeDiv x 0 = Nothing
+safeDiv x y = Just $ x / y
+
+div3 :: Float -> Float -> Float -> Maybe Float
+div3 x y z = do
+    xDivY <- safeDiv x y
+    safeDiv xDivY z
